3bb849836eb95d9ce228eb7d254d10ba7290e1c8
Bhadraraj <20bhadri2001@gmail.com>
2026-03-09 16:38:55 -0400

    Updates

---
+++ _next/static/chunks/app/shop/page-4f94f7534e71fd0c.js
@@ -0,0 +1,6 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[21],{2482:function(e,n,t){"use strict";t.d(n,{Z:function(){return u}});var s=t(5531);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let u=(0,s.Z)("Filter",[["polygon",{points:"22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3",key:"1yg77f"}]])},5397:function(e,n,t){Promise.resolve().then(t.bind(t,6279))},6279:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return l}});var s=t(7437),u=t(646),c=t(1438),r=t(920),i=t(4033);function l(){let e=(0,i.useRouter)();return(0,s.jsxs)("div",{className:"min-h-screen flex flex-col",children:[(0,s.jsx)(u.default,{}),(0,s.jsx)("main",{className:"flex-1",children:(0,s.jsx)(r.Z,{onVendorSelect:n=>e.push("/shop/".concat(n))})}),(0,s.jsx)(c.default,{})]})}}},function(e){e.O(0,[966,175,448,920,971,864,744],function(){return e(e.s=5397)}),_N_E=e.O()}]);
